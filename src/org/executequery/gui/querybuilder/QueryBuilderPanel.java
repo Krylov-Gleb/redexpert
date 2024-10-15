c81125a68b5e519cb36db9005b1cf8835710574d
Krylov-Gleb <glebkrylov68@gmail.com>
2024-10-15 10:23:25 +0300

    Fixed: The class structure and added comments and authorship.
    Added: Added work with tables (creation and deletion) and SQL queries.
    
    Deleted: Deleted code fragments that are not in use.

--- src/org/executequery/gui/querybuilder/QueryBuilderPanel.java
+++ src/org/executequery/gui/querybuilder/QueryBuilderPanel.java
@@ -2,47 +2,143 @@ package org.executequery.gui.querybuilder;
 
 import org.executequery.base.TabView;
 import org.executequery.gui.editor.QueryEditor;
+import org.executequery.gui.querybuilder.WorkQuryEditor.CreateStringQuery;
 import org.executequery.gui.querybuilder.inputPanel.QueryBuilderInputPanel;
 import org.executequery.gui.querybuilder.toolBar.QueryBuilderToolBarPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
+
+/**
+ * The QueryBuilderPanel
+ *
+ * @author Krylov Gleb
+ */
 
 public class QueryBuilderPanel extends JPanel implements TabView {
+
+    // --- Constants ---
 
     public static final String TITLE = "Query Builder";
     public static final String FRAME_ICON = "icon_table_validation";
 
+    private ArrayList<JTable> tableListInInputPanel;
+
+    // --- GUI Components ---
+
     private JPanel mainPanel;
-    private QueryBuilderToolBarPanel toolBarPanel;
+    private QueryBuilderToolBarPanel queryBuilderToolBarPanel;
     private QueryBuilderInputPanel inputElementPanel;
-
+    private CreateStringQuery createStringQuery;
     private QueryEditor queryEditor;
-    private JSplitPane splitPane;
 
-    public QueryBuilderPanel(){
+    // --- Designer ---
+
+    public QueryBuilderPanel() {
         init();
     }
 
-    private void init(){
+    /**
+     * Method for initialization
+     */
+    private void init() {
 
         mainPanel = new JPanel(new BorderLayout());
         queryEditor = new QueryEditor();
-        toolBarPanel = new QueryBuilderToolBarPanel();
-        inputElementPanel = new QueryBuilderInputPanel();
+        queryBuilderToolBarPanel = new QueryBuilderToolBarPanel(this);
+        tableListInInputPanel = new ArrayList<>();
 
-        splitPane = new JSplitPane(SwingConstants.VERTICAL,queryEditor,inputElementPanel);
+        createStringQuery = new CreateStringQuery(this);
+        inputElementPanel = new QueryBuilderInputPanel();
+        inputElementPanel.setLayout(new GridBagLayout());
 
         arrangeComponent();
     }
 
-    private void arrangeComponent(){
-
-        mainPanel.add(toolBarPanel,BorderLayout.NORTH);
-        mainPanel.add(splitPane,BorderLayout.CENTER);
+    /**
+     * A method for placing components
+     */
+    private void arrangeComponent() {
+        mainPanel.add(inputElementPanel, BorderLayout.CENTER);
+        mainPanel.add(queryBuilderToolBarPanel, BorderLayout.NORTH);
 
         setLayout(new BorderLayout());
-        add(mainPanel,BorderLayout.CENTER);
+        add(mainPanel, BorderLayout.CENTER);
+    }
+
+    /**
+     * A method for adding a table to the component output panel (InputPanel)
+     *
+     * @param scrollPane Passing the ScrollPane in which the table is stored
+     */
+    public void addTableInInputPanel(JScrollPane scrollPane) {
+
+        // The table is wrapped in a scroll pane so that the headings are reflected on it
+        inputElementPanel.add(scrollPane);
+
+        // Methods for creating a real-time table
+        inputElementPanel.revalidate();
+        inputElementPanel.repaint();
+    }
+
+    /**
+     * Method for deleting a table from the element output panel (InputPanel)
+     */
+    public void removeTableInInputPanel() {
+
+        // I get an array of components on the output panel (InputPanel)
+        if (inputElementPanel.getComponents().length > 0) {
+            // Deleting the table
+            inputElementPanel.remove(inputElementPanel.getComponents().length - 1);
+        }
+        inputElementPanel.revalidate();
+        inputElementPanel.repaint();
+    }
+
+    /**
+     * Method for getting the output panel (InputPanel)
+     *
+     * @return JPanel
+     */
+    public JPanel getInputPanel() {
+        return inputElementPanel;
+    }
+
+    /**
+     * Returns the created QueryEditor
+     *
+     * @return QueryEditor
+     */
+    public QueryEditor getQueryEditor() {
+        return queryEditor;
+    }
+
+    /**
+     * The table added to the output panel is added to the array (ArrayList).
+     *
+     * @param table
+     */
+    public void addListTableInInputPanel(JTable table) {
+        tableListInInputPanel.add(table);
+    }
+
+    /**
+     * Deletes the table deleted from the output panel from the array (ArrayList)
+     */
+    public void removeListTableInInputPanel() {
+        if (tableListInInputPanel.size() > 0) {
+            tableListInInputPanel.remove(tableListInInputPanel.size() - 1);
+        }
+    }
+
+    /**
+     * Returns tables located on the output panel (InputPanel)
+     *
+     * @return ArrayList<JTable>
+     */
+    public ArrayList<JTable> getListTableInInputPanel() {
+        return tableListInInputPanel;
     }
 
     @Override
