4d9f31b82fe4ccc9c168a8d8f9b39dc4c6d6b7b6
Krylov-Gleb <glebkrylov68@gmail.com>
2024-10-15 10:28:37 +0300

    Fixed: Improved class structure, added comments and attribution.
    Added: Nothing has been added.
    
    Deleted: Unused code snippets have been deleted.

--- src/org/executequery/gui/querybuilder/inputPanel/QueryBuilderInputPanel.java
+++ src/org/executequery/gui/querybuilder/inputPanel/QueryBuilderInputPanel.java
@@ -3,16 +3,30 @@ package org.executequery.gui.querybuilder.inputPanel;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * The QueryBuilderInputPanel
+ *
+ * @author Krylov Gleb
+ */
+
 public class QueryBuilderInputPanel extends JPanel {
+
+    // --- Designer ---
 
     public QueryBuilderInputPanel(){
         init();
     }
 
+    /**
+     * Method for initialization
+     */
     private void init(){
         arrangeComponent();
     }
 
+    /**
+     * A method for placing components
+     */
     private void arrangeComponent(){
         setBorder(BorderFactory.createLineBorder(Color.GRAY));
     }
