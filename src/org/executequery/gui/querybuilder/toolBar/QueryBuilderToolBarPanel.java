5615c1ac17e53b2c69e78de014dfcf355b6d11d3
Krylov-Gleb <glebkrylov68@gmail.com>
2024-10-15 10:32:58 +0300

    Fixed: Improved class structure, added comments and attribution.
    Added: Buttons and functionality for adding and deleting tables as well as creating queries via (QueryEditor).
    
    Deleted: Unused code snippets have been deleted.

--- src/org/executequery/gui/querybuilder/toolBar/QueryBuilderToolBarPanel.java
+++ src/org/executequery/gui/querybuilder/toolBar/QueryBuilderToolBarPanel.java
@@ -1,62 +1,115 @@
 package org.executequery.gui.querybuilder.toolBar;
 
-import org.underworldlabs.swing.layouts.GridBagHelper;
+import org.executequery.gui.WidgetFactory;
+import org.executequery.gui.querybuilder.ExecuteQueryEditor;
+import org.executequery.gui.querybuilder.QueryBuilderPanel;
+import org.executequery.gui.querybuilder.WorkQuryEditor.CreateStringQuery;
+import org.executequery.gui.querybuilder.buttonPanel.AddTableQueryBuilder;
+import org.underworldlabs.swing.ConnectionsComboBox;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 
-public class QueryBuilderToolBarPanel extends JPanel {
+/**
+ * The QueryBuilderToolBarPanel
+ *
+ * @author Krylov Gleb
+ */
 
+public class QueryBuilderToolBarPanel extends JToolBar {
+
+    // --- Fields ---
+
+    private CreateStringQuery createStringQuery;
+
+    // --- GUI Components ---
+
+    private JButton addTable;
     private JButton selectButton;
     private JButton joinButton;
-    private JButton columnsButton;
-    private JButton sortingButton;
-    private JButton miscellaneousButton;
+    private JButton columnButton;
+    private JButton conditionButton;
+    private JButton createQuery;
+    private JButton removeTable;
+    private ConnectionsComboBox connectionsCombo;
+    private QueryBuilderPanel queryBuilderPanel;
 
-    public QueryBuilderToolBarPanel(){
+    // --- Designer ---
+
+    public QueryBuilderToolBarPanel(QueryBuilderPanel queryBuilderPanel) {
+        this.queryBuilderPanel = queryBuilderPanel;
         init();
     }
 
-    private void init(){
+    /**
+     * Method for initialization
+     */
+    private void init() {
+        createStringQuery = new CreateStringQuery(queryBuilderPanel);
 
-        selectButton = new JButton("Select");
-        selectButton.addActionListener(event -> {
+        // --- Button ---
+
+        addTable = WidgetFactory.createButton("Add Table", "Add Table", event -> {
+            AddTableQueryBuilder addTableQueryBuilder = new AddTableQueryBuilder(queryBuilderPanel);
+            addTableQueryBuilder.setVisible(true);
+            addTableQueryBuilder.setSize(400, 300);
+        });
+
+        removeTable = WidgetFactory.createButton("Remove Table", "Remove Table", event -> {
+            queryBuilderPanel.removeTableInInputPanel();
+            queryBuilderPanel.removeListTableInInputPanel();
+        });
+
+        selectButton = WidgetFactory.createButton("Select Button", "Select");
+        joinButton = WidgetFactory.createButton("Join Button", "Join");
+        columnButton = WidgetFactory.createButton("Column Button", "Column");
+        conditionButton = WidgetFactory.createButton("Condition Button", "Condition");
+
+        createQuery = WidgetFactory.createButton("Create Query", "Create Query", event -> {
+
+            ArrayList<JTable> arrayTableInInputPanel = queryBuilderPanel.getListTableInInputPanel();
+
+            if (!arrayTableInInputPanel.isEmpty()) {
+
+                ArrayList<String> Attribute = new ArrayList<>();
+
+                createStringQuery.setTables(arrayTableInInputPanel.get(0).getColumnName(0));
+
+                for (int i = 0; i < arrayTableInInputPanel.get(0).getRowCount(); i++) {
+                    if ((boolean) arrayTableInInputPanel.get(0).getValueAt(i, 1)) {
+                        Attribute.add((String) arrayTableInInputPanel.get(0).getValueAt(i, 0));
+                    }
+                }
+
+                createStringQuery.setAttribute(Attribute);
+                ExecuteQueryEditor executeQueryEditor = new ExecuteQueryEditor(createStringQuery);
+
+            }
 
         });
 
-        joinButton = new JButton("Join");
-        joinButton.addActionListener(event -> {
-
-        });
-
-        columnsButton = new JButton("Columns");
-        columnsButton.addActionListener(event ->{
-
-        });
-
-        sortingButton = new JButton("Sorting");
-        sortingButton.addActionListener(event -> {
-
-        });
-
-        miscellaneousButton = new JButton("Miscellaneous");
-        miscellaneousButton.addActionListener(event -> {
-
-        });
+        // ComboBox to view the current connection
+        connectionsCombo = WidgetFactory.createConnectionComboBox("connectionsCombo", true);
+        connectionsCombo.setMaximumSize(new Dimension(200, 30));
+        connectionsCombo.setPreferredSize(new Dimension(connectionsCombo.getWidth(), 30));
 
         arrangeComponent();
     }
 
-    private void arrangeComponent(){
-
-        setLayout(new GridBagLayout());
-        setBorder(BorderFactory.createLineBorder(Color.GRAY));
-        add(selectButton,new GridBagHelper().setX(0).setY(0).anchorWest().fillHorizontally().setInsets(5,5,5,5).get());
-        add(joinButton,new GridBagHelper().setX(10).setY(0).anchorWest().fillHorizontally().setInsets(5,5,5,5).get());
-        add(columnsButton,new GridBagHelper().setX(20).setY(0).anchorWest().fillHorizontally().setInsets(5,5,5,5).get());
-        add(sortingButton,new GridBagHelper().setX(30).setY(0).anchorWest().fillHorizontally().setInsets(5,5,5,5).get());
-        add(miscellaneousButton,new GridBagHelper().setX(40).setY(0).anchorWest().fillHorizontally().setInsets(5,5,5,5).get());
-
+    /**
+     * A method for placing components
+     */
+    private void arrangeComponent() {
+        setPreferredSize(new Dimension(getWidth(), 45));
+        add(connectionsCombo);
+        add(addTable);
+        add(removeTable);
+        add(selectButton);
+        add(joinButton);
+        add(columnButton);
+        add(conditionButton);
+        add(createQuery);
     }
 
 }
