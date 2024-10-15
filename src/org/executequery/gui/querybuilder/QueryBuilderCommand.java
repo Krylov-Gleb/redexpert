eb18c2680104a8f88a6b4d02a2e8ea95249a34e1
Krylov-Gleb <glebkrylov68@gmail.com>
2024-10-15 10:26:36 +0300

    Fixed: Improved class structure, added comments and attribution.
    Added: Nothing has been added.
    
    Deleted: Unused code snippets have been deleted.

--- src/org/executequery/gui/querybuilder/QueryBuilderCommand.java
+++ src/org/executequery/gui/querybuilder/QueryBuilderCommand.java
@@ -2,13 +2,23 @@ package org.executequery.gui.querybuilder;
 
 import org.executequery.GUIUtilities;
 import org.executequery.actions.OpenFrameCommand;
-import org.executequery.gui.editor.QueryEditor;
 import org.underworldlabs.swing.actions.BaseCommand;
 
 import java.awt.event.ActionEvent;
 
+/**
+ * The QueryBuilderCommand
+ *
+ * @author Krylov Gleb
+ */
+
 public class QueryBuilderCommand extends OpenFrameCommand implements BaseCommand {
 
+    /**
+     * The method that adjusts the display and shows the panel (QueryBuilderPanel)
+     *
+     * @param queryBuilderPanel
+     */
     private void showPanel(QueryBuilderPanel queryBuilderPanel) {
 
         String title = QueryBuilderPanel.TITLE;
