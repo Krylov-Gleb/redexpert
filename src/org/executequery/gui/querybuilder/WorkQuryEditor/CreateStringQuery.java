089d729fb514080a787a4caea7d1a4e5a99b19ba
Krylov-Gleb <glebkrylov68@gmail.com>
2024-10-21 14:51:58 +0300

    Changes: Comments: Old comments have been rewritten in a more understandable and readable form. New ones have been added. Fields: Fixed the field structure. Style: Fixed the writing style.
    Additions: The old code has been added and bugs have been fixed. And new functionality has also been written.
    
    Comments: No.

--- src/org/executequery/gui/querybuilder/WorkQuryEditor/CreateStringQuery.java
+++ src/org/executequery/gui/querybuilder/WorkQuryEditor/CreateStringQuery.java
@@ -1,77 +1,243 @@
 package org.executequery.gui.querybuilder.WorkQuryEditor;
 
-import org.executequery.gui.querybuilder.QueryBuilderPanel;
-
-import java.awt.*;
+import javax.swing.*;
 import java.util.ArrayList;
 
 /**
- * The CreateStringQuery
+ * A class for dynamically composing a query that the user will use. (query constructor)
  *
  * @author Krylov Gleb
  */
-
 public class CreateStringQuery {
 
-    // --- Constants ---
-
+    // --- Constant field ---
     private final String SELECT = "SELECT";
     private final String FROM = "FROM";
+    private final String ON = "ON";
+    private final String EMPTINESS = "";
+    private final String WHITESPACE = " ";
+    private final String POINT = ".";
+    private final String EQUALS = "=";
+    private final String COMMA = ",";
+    private final String STAR = "*";
 
-    // --- Other fields ---
-
-    private String ATTRIBUTE;
-    private String TABLES;
-    private QueryBuilderPanel queryBuilderPanel;
-    private Component[] componentsInInputPanel;
-
-    // --- Designer ---
-
-    public CreateStringQuery(QueryBuilderPanel queryBuilderPanel) {
-        this.queryBuilderPanel = queryBuilderPanel;
-        init();
-    }
-
-    /**
-     * Method for initialization
-     */
-    private void init() {
-        ATTRIBUTE = "*";
-        TABLES = "EMPLOYEE";
-    }
+    // --- Other field ---
+    private String first = "";
+    private String skip = "";
+    private String distinct = "ALL";
+    private String join = "";
+    private String table = "EMPLOYEE";
+    private String attribute = "";
 
     /**
-     * A method that returns a composite request
+     * A method for adding the first value to the query.
      *
-     * @return String (Query)
+     * @param number
      */
-    public String getQuery() {
-        return SELECT + " " + ATTRIBUTE + " " + FROM + " " + TABLES;
+    public void addFirst(String number) {
+        if (!number.isEmpty()) {
+            first = "FIRST " + number;
+        } else {
+            first = EMPTINESS;
+        }
     }
 
     /**
-     * The method for setting the table in the query
-     *
-     * @param NameTable Replacement table
+     * A method for clearing (deleting) the first value from the query.
      */
-    public void setTables(String NameTable) {
-        TABLES = NameTable;
+    public void clearFirst() {
+        first = EMPTINESS;
     }
 
     /**
-     * Method for setting attributes in a request
+     * A method for adding skip values to a query.
      *
-     * @param Array List of attributes
+     * @param number
      */
-    public void setAttribute(ArrayList<String> Array) {
-        String Attributes = "";
-        for (int i = 0; i < Array.size(); i++) {
-            if (i != Array.size() - 1) {
-                Attributes = Attributes + Array.get(i) + ",";
+    public void addSkip(String number) {
+        if (!number.isEmpty()) {
+            skip = "SKIP " + number;
+        } else {
+            skip = EMPTINESS;
+        }
+    }
+
+    /**
+     * A method for clearing (deleting) skip values from a query.
+     */
+    public void clearSkip() {
+        skip = EMPTINESS;
+    }
+
+    /**
+     * A method for adding a distinct value to a query.
+     *
+     * @param distinct
+     */
+    public void setDistinct(String distinct) {
+        this.distinct = distinct;
+    }
+
+    /**
+     * A method for adding attributes to a request.
+     * Also, if the user added attributes from several tables and did not specify a join,
+     * then this method will create not only attributes but also a natural join.
+     *
+     * @param attributes
+     * @param queryTable
+     */
+    public void addAttributes(ArrayList<ArrayList<String>> attributes, ArrayList<String> queryTable) {
+        StringBuilder stringBuilder = new StringBuilder(EMPTINESS);
+
+        for (int i = 0; i < attributes.size(); i++) {
+            if (i >= 1) {
+                if(!join.contains(queryTable.get(i))){
+                    JOptionPane.showMessageDialog(new JFrame(),"Пожалуйста укажите соединение (Join) для таблиц!","Отсутствует объединение (Join)",JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+            }
+            if (i < attributes.size()-1) {
+                stringBuilder.append(setAttribute(attributes.get(i))).append(COMMA);
             } else {
-                Attributes = Attributes + Array.get(i);
+                stringBuilder.append(setAttribute(attributes.get(i)));
             }
         }
-        ATTRIBUTE = Attributes;
+
+        attribute = stringBuilder.toString();
+
+    }
+
+    /**
+     * Clears (removes) all attributes from the query.
+     * Replaces them with *.
+     */
+    public void clearAttribute() {
+        attribute = STAR;
+    }
+
+    /**
+     * This method creates the specified join between the two passed tables
+     * and also connects them using the key parameters passed to the method.
+     *
+     * @param tableNameOne
+     * @param tableNameTwo
+     * @param joinName
+     * @param keyAttributeOne
+     * @param keyAttributeTwo
+     */
+    public void addJoins(String tableNameOne, String tableNameTwo, String joinName, String keyAttributeOne, String keyAttributeTwo) {
+
+        StringBuilder stringBuilder = new StringBuilder(join);
+
+        if (stringBuilder.indexOf(WHITESPACE + tableNameOne + WHITESPACE) == -1) {
+            if (stringBuilder.indexOf(WHITESPACE + tableNameTwo + WHITESPACE) == -1) {
+                stringBuilder.replace(0,stringBuilder.length(),EMPTINESS);
+                stringBuilder.append(tableNameOne).append(WHITESPACE).append(joinName.toUpperCase()).append(WHITESPACE).append(tableNameTwo).append(WHITESPACE).append(ON).append(WHITESPACE).append(tableNameOne).append(POINT).append(keyAttributeOne).append(EQUALS).append(tableNameTwo).append(POINT).append(keyAttributeTwo);
+                join = stringBuilder.toString();
+                return;
+
+            } else {
+                stringBuilder.append(WHITESPACE).append(joinName.toUpperCase()).append(WHITESPACE).append(tableNameOne).append(WHITESPACE).append(ON).append(WHITESPACE).append(tableNameOne).append(POINT).append(keyAttributeOne).append(EQUALS).append(tableNameTwo).append(POINT).append(keyAttributeTwo);
+                join = stringBuilder.toString();
+                return;
+            }
+        }
+
+        if (stringBuilder.indexOf(WHITESPACE + tableNameTwo + WHITESPACE) == -1) {
+            if (stringBuilder.indexOf(WHITESPACE + tableNameOne + WHITESPACE) == -1) {
+                stringBuilder.replace(0,stringBuilder.length(),EMPTINESS);
+                stringBuilder.append(tableNameOne).append(WHITESPACE).append(joinName.toUpperCase()).append(WHITESPACE).append(tableNameTwo).append(WHITESPACE).append(ON).append(WHITESPACE).append(tableNameOne).append(POINT).append(keyAttributeOne).append(EQUALS).append(tableNameTwo).append(POINT).append(keyAttributeTwo);
+                join = stringBuilder.toString();
+                return;
+
+            } else {
+                stringBuilder.append(WHITESPACE).append(joinName.toUpperCase()).append(WHITESPACE).append(tableNameTwo).append(WHITESPACE).append(ON).append(WHITESPACE).append(tableNameOne).append(POINT).append(keyAttributeOne).append(EQUALS).append(tableNameTwo).append(POINT).append(keyAttributeTwo);
+                join = stringBuilder.toString();
+                return;
+            }
+        }
+    }
+
+    /**
+     * Cleans (removes) join.
+     */
+    public void clearJoin() {
+        join = EMPTINESS;
+    }
+
+    /**
+     * Adds a table to the query.
+     *
+     * @param Table
+     */
+    public void addTable(String Table) {
+        this.table = Table;
+    }
+
+    /**
+     * Clears (deletes) the table from the query.
+     */
+    public void clearTable() {
+        this.table = "EMPLOYEE";
+    }
+
+    /**
+     * Compiles and returns a query.
+     *
+     * @return
+     */
+    public String getQuery() {
+        StringBuilder Query = new StringBuilder(WHITESPACE);
+
+        Query.append(SELECT).append(WHITESPACE);
+
+        if (!first.isEmpty()) {
+            Query.append(first).append(WHITESPACE);
+        }
+
+        if (!skip.isEmpty()) {
+            Query.append(skip).append(WHITESPACE);
+        }
+
+        if (attribute.isEmpty()) {
+            attribute = STAR;
+        }
+
+        Query.append(distinct).append(WHITESPACE).append(attribute).append(WHITESPACE).append(FROM);
+
+        if (!join.isEmpty()) {
+            if(join.charAt(0) != ' ') {
+                join = WHITESPACE + join;
+            }
+            Query.append(join).append(WHITESPACE);
+        } else {
+            if(table.charAt(0) != ' ') {
+                table = WHITESPACE + table;
+            }
+            Query.append(table).append(WHITESPACE);
+        }
+
+        Query.append(";");
+
+        return Query.toString();
+    }
+
+    /**
+     * It helps to create a carriage list of attributes and turns them into one line.
+     *
+     * @param Array
+     * @return
+     */
+    private String setAttribute(ArrayList<String> Array) {
+        StringBuilder stringBuilderAttributes = new StringBuilder(EMPTINESS);
+
+        for (int i = 0; i < Array.size(); i++) {
+            if (i != Array.size() - 1) {
+                stringBuilderAttributes.append(Array.get(i)).append(COMMA);
+            } else {
+                stringBuilderAttributes.append(Array.get(i));
+            }
+        }
+        return stringBuilderAttributes.toString();
     }
 }
