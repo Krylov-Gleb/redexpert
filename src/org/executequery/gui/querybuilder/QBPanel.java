0c86180f5d06882413b0fca2310e1e72536f7da5
Krylov-Gleb <glebkrylov68@gmail.com>
2024-12-30 13:27:24 +0300

    Added a block representation of the query.

--- src/org/executequery/gui/querybuilder/QBPanel.java
+++ src/org/executequery/gui/querybuilder/QBPanel.java
@@ -7,6 +7,7 @@ import org.executequery.gui.editor.QueryEditorTextPanel;
 import org.executequery.localization.Bundles;
 
 import javax.swing.*;
+import javax.swing.border.CompoundBorder;
 import javax.swing.table.JTableHeader;
 import java.awt.*;
 import java.util.ArrayList;
@@ -30,13 +31,23 @@ public class QBPanel extends JPanel implements TabView {
     // --- Компоненты графического интерфейса ---
 
     private JPanel panelPlacingComponents;
+    private JSplitPane mainSplitPanePanel;
     private JSplitPane splitPanePanelGUIComponentsAndPanelTestingQuery;
     private QueryEditorTextPanel panelTestingQuery;
+    private JPanel blocksPanel;
     private JPanel panelGUIComponents;
-    private JScrollPane scrollPanePlacingComponents;
+    private JScrollPane scrollPanePlacingBlocks;
+    private JScrollPane scrollPanePlacingGUIComponents;
     private QBToolBar toolBar;
     private ArrayList<JTable> listTablesPanelGUIComponents;
     private QueryConstructor queryConstructor;
+
+    // --- Other fields ---
+    // --- Остальные поля ---
+
+    private int locationNewTableX = 50;
+    private int locationNewTableY = 50;
+    private ArrayList<String> arrayListCoordinateDeleteComponents;
 
     /**
      * Creating the main panel of the query builder.
@@ -60,7 +71,6 @@ public class QBPanel extends JPanel implements TabView {
         initToolBar();
         initQueryEditorTextPanel();
         initArrays();
-        initPanelGUIComponents();
         intScrollPane();
         initSplitPane();
         arrangeComponent();
@@ -74,6 +84,15 @@ public class QBPanel extends JPanel implements TabView {
     private void initPanel() {
         panelPlacingComponents = WidgetFactory.createPanel("panelPlacingComponents");
         panelPlacingComponents.setLayout(new BorderLayout());
+
+        blocksPanel = WidgetFactory.createPanel("blocksPanel");
+        blocksPanel.setLayout(new BoxLayout(blocksPanel,BoxLayout.Y_AXIS));
+        blocksPanel.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
+
+        panelGUIComponents = WidgetFactory.createPanel("panelGUIComponents");
+        panelGUIComponents.setPreferredSize(new Dimension(100000,100000));
+        panelGUIComponents.setLayout(null);
+        panelGUIComponents.setBorder(BorderFactory.createLineBorder(Color.GRAY));
     }
 
     /**
@@ -102,6 +121,7 @@ public class QBPanel extends JPanel implements TabView {
     private void initQueryEditorTextPanel() {
         panelTestingQuery = new QueryEditorTextPanel(new QueryEditor());
         panelTestingQuery.getQueryArea().setEditable(false);
+        panelTestingQuery.setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY));
     }
 
     /**
@@ -111,6 +131,7 @@ public class QBPanel extends JPanel implements TabView {
      */
     private void initArrays() {
         listTablesPanelGUIComponents = new ArrayList<>();
+        arrayListCoordinateDeleteComponents = new ArrayList<>();
     }
 
     /**
@@ -119,8 +140,11 @@ public class QBPanel extends JPanel implements TabView {
      * Метод для инициализации SplitPane.
      */
     private void initSplitPane() {
-        splitPanePanelGUIComponentsAndPanelTestingQuery = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,panelTestingQuery,scrollPanePlacingComponents);
-        splitPanePanelGUIComponentsAndPanelTestingQuery.setDividerLocation(650);
+        splitPanePanelGUIComponentsAndPanelTestingQuery = new JSplitPane(JSplitPane.VERTICAL_SPLIT,scrollPanePlacingGUIComponents,panelTestingQuery);
+        splitPanePanelGUIComponentsAndPanelTestingQuery.setDividerLocation(550);
+
+        mainSplitPanePanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,splitPanePanelGUIComponentsAndPanelTestingQuery, scrollPanePlacingBlocks);
+        mainSplitPanePanel.setDividerLocation(800);
     }
 
     /**
@@ -129,19 +153,11 @@ public class QBPanel extends JPanel implements TabView {
      * Метод для инициализации ScrollPane.
      */
     private void intScrollPane() {
-        scrollPanePlacingComponents = new JScrollPane();
-        scrollPanePlacingComponents.setViewportView(panelGUIComponents);
-    }
+        scrollPanePlacingBlocks = new JScrollPane();
+        scrollPanePlacingBlocks.setViewportView(blocksPanel);
 
-    /**
-     * A method for initializing the graphic component layout panel.
-     * <p>
-     * Метод для инициализации панели расположения графических компонентов.
-     */
-    private void initPanelGUIComponents() {
-        panelGUIComponents = WidgetFactory.createPanel("panelGUIComponents");
-        panelGUIComponents.setLayout(new BoxLayout(panelGUIComponents,BoxLayout.Y_AXIS));
-        panelGUIComponents.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        scrollPanePlacingGUIComponents = new JScrollPane();
+        scrollPanePlacingGUIComponents.setViewportView(panelGUIComponents);
     }
 
     /**
@@ -170,18 +186,18 @@ public class QBPanel extends JPanel implements TabView {
      * Метод для размещения компонентов в панели для размещения компонентов.
      */
     private void arrangeComponentsInPanelForPlacingComponents() {
-        panelPlacingComponents.add(splitPanePanelGUIComponentsAndPanelTestingQuery, BorderLayout.CENTER);
+        panelPlacingComponents.add(mainSplitPanePanel, BorderLayout.CENTER);
         panelPlacingComponents.add(toolBar, BorderLayout.NORTH);
     }
 
-    public void addBlockInPanelBlocks(JPanel panel){
-        panelGUIComponents.add(panel);
-        panelGUIComponents.revalidate();
-        panelGUIComponents.repaint();
+    public void addBlockInPanelBlocks(JLabel label){
+        blocksPanel.add(label);
+        blocksPanel.revalidate();
+        blocksPanel.repaint();
     }
 
     public void clearBlocksPanel(){
-        panelGUIComponents.removeAll();
+        blocksPanel.removeAll();
     }
 
     /**
@@ -189,8 +205,8 @@ public class QBPanel extends JPanel implements TabView {
      * <p>
      * Метод для получения панели вывода элементов.
      */
-    public JPanel getPanelGUIComponents() {
-        return panelGUIComponents;
+    public JPanel getBlocksPanel() {
+        return blocksPanel;
     }
 
     /**
@@ -257,7 +273,6 @@ public class QBPanel extends JPanel implements TabView {
         return panelTestingQuery.getQueryAreaText();
     }
 
-
     /**
      * The method for applying the test query.
      * <p>
@@ -265,6 +280,173 @@ public class QBPanel extends JPanel implements TabView {
      */
     public void applyTestQuery(QueryEditor queryEditor) {
         queryEditor.setEditorText(panelTestingQuery.getQueryAreaText());
+    }
+
+    public void addBlockInBlocksPanel(String text,Color colorBorder,Color colorBackground){
+        JLabel label = new JLabel();
+        label.setFont(new Font("Arial",Font.BOLD,18));
+        label.setOpaque(true);
+        label.setBackground(colorBackground);
+        label.setText(text);
+        label.setBorder(new CompoundBorder(BorderFactory.createLineBorder(colorBorder,5,true),BorderFactory.createEmptyBorder(10,10,10,10)));
+        addBlockInPanelBlocks(label);
+    }
+
+    /**
+     * A method for adding a table to the output panel of graphical components.
+     * <p>
+     * Метод для добавления таблицы на панель вывода графических компонентов.
+     */
+    public void addTableInPanelGUIComponents(JPanel panel) {
+        JScrollPane scrollPane = (JScrollPane) panel.getComponent(0);
+        if (isTableUnique(scrollPane)) {
+            if (arrayListCoordinateDeleteComponents.isEmpty()) {
+                if (panelGUIComponents.getComponents().length > 0) {
+                    if (locationNewTableX >= 800) {
+                        locationNewTableX = 50;
+                        locationNewTableY = locationNewTableY + 250;
+                    } else {
+                        locationNewTableX = locationNewTableX + 250;
+                    }
+                } else {
+                    locationNewTableX = 50;
+                    locationNewTableY = 50;
+                }
+                if (locationNewTableX == 50) {
+                    JViewport viewport = scrollPane.getViewport();
+                    JTable table = (JTable) viewport.getComponent(0);
+                    JTableHeader header = table.getTableHeader();
+                    header.setBackground(new Color(255, 91, 91));
+                    header.setForeground(Color.WHITE);
+                }
+                if (locationNewTableX == 300) {
+                    JViewport viewport = scrollPane.getViewport();
+                    JTable table = (JTable) viewport.getComponent(0);
+                    JTableHeader header = table.getTableHeader();
+                    header.setBackground(new Color(174, 0, 0));
+                    header.setForeground(Color.WHITE);
+                }
+                if (locationNewTableX == 550) {
+                    JViewport viewport = scrollPane.getViewport();
+                    JTable table = (JTable) viewport.getComponent(0);
+                    JTableHeader header = table.getTableHeader();
+                    header.setBackground(new Color(116, 2, 2));
+                    header.setForeground(Color.WHITE);
+                }
+                if (locationNewTableX == 800) {
+                    JViewport viewport = scrollPane.getViewport();
+                    JTable table = (JTable) viewport.getComponent(0);
+                    JTableHeader header = table.getTableHeader();
+                    header.setBackground(new Color(61, 0, 0));
+                    header.setForeground(Color.WHITE);
+                }
+                panel.setBounds(locationNewTableX, locationNewTableY, 220, 220);
+            } else {
+                if (panelGUIComponents.getComponents().length == 0) {
+                    arrayListCoordinateDeleteComponents.clear();
+                    locationNewTableX = 50;
+                    locationNewTableY = 50;
+                    if (locationNewTableX == 50) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(255, 91, 91));
+                        header.setForeground(Color.WHITE);
+                    }
+                    if (locationNewTableX == 300) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(174, 0, 0));
+                        header.setForeground(Color.WHITE);
+                    }
+                    if (locationNewTableX == 550) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(116, 2, 2));
+                        header.setForeground(Color.WHITE);
+                    }
+                    if (locationNewTableX == 800) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(61, 0, 0));
+                        header.setForeground(Color.WHITE);
+                    }
+                    panel.setBounds(locationNewTableX, locationNewTableY, 220, 220);
+                } else {
+                    if (Integer.parseInt(arrayListCoordinateDeleteComponents.get(0).split(",")[0]) == 50) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(255, 91, 91));
+                        header.setForeground(Color.WHITE);
+                    }
+                    if (Integer.parseInt(arrayListCoordinateDeleteComponents.get(0).split(",")[0]) == 300) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(174, 0, 0));
+                        header.setForeground(Color.WHITE);
+                    }
+                    if (Integer.parseInt(arrayListCoordinateDeleteComponents.get(0).split(",")[0]) == 550) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(116, 2, 2));
+                        header.setForeground(Color.WHITE);
+                    }
+                    if (Integer.parseInt(arrayListCoordinateDeleteComponents.get(0).split(",")[0]) == 800) {
+                        JViewport viewport = scrollPane.getViewport();
+                        JTable table = (JTable) viewport.getComponent(0);
+                        JTableHeader header = table.getTableHeader();
+                        header.setBackground(new Color(61, 0, 0));
+                        header.setForeground(Color.WHITE);
+                    }
+                    panel.setBounds(Integer.parseInt(arrayListCoordinateDeleteComponents.get(0).split(",")[0]), Integer.parseInt(arrayListCoordinateDeleteComponents.get(0).split(",")[1]), 220, 220);
+                    arrayListCoordinateDeleteComponents.remove(0);
+                }
+            }
+        }
+        panelGUIComponents.add(panel);
+        panelGUIComponents.revalidate();
+        panelGUIComponents.repaint();
+    }
+
+    /**
+     * A method for removing a table from the output panel of graphical components.
+     * <p>
+     * Метод для удаления таблицы из панели вывода графических компонентов.
+     */
+    public void removeTableInInputPanel(String nameTable) {
+        for (int i = 0; i < getListTable().size(); i++) {
+            if (nameTable.equals(getListTable().get(i).getColumnName(0))) {
+                JPanel panel = (JPanel) panelGUIComponents.getComponent(i);
+                arrayListCoordinateDeleteComponents.add(panel.getX() + "," + panel.getY());
+                panelGUIComponents.remove(i);
+                panelGUIComponents.revalidate();
+                panelGUIComponents.repaint();
+            }
+        }
+    }
+    /**
+     * A method for checking the table for uniqueness.
+     * <p>
+     * Метод для проверки таблицы на уникальность.
+     */
+    private boolean isTableUnique(JScrollPane scrollPane) {
+        boolean Check = true;
+        for (int i = 0; i < panelGUIComponents.getComponents().length; i++) {
+            if (panelGUIComponents.getComponents()[i] == scrollPane) {
+                Check = false;
+            }
+        }
+        return Check;
+    }
+
+    public JPanel getPanelGUIComponents(){
+        return panelGUIComponents;
     }
 
     @Override
